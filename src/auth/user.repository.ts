44792a913962d1b90db385af87927e8ffd09ade8
Kaushik7984 <kaushik7984@gmail.com>
2025-04-05 16:20:23 +0530

    create SignUp api

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,11 @@
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credential.dto';
 
 export class UserRepository extends Repository<User> {
-  //
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    const user = this.create({ username, password });
+    await this.save(user);
+  }
 }
