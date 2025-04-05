901e8e7e54f9f66d7af21d05e7079837877c859f
Kaushik7984 <kaushik7984@gmail.com>
2025-04-05 15:55:09 +0530

    setting up auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { UserRepository } from './user.repository';
+import { DataSource } from 'typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [
+    AuthService,
+    {
+      provide: UserRepository,
+      useFactory: (DataSource: DataSource) => {
+        const baseRepo = DataSource.getRepository(User);
+        Object.setPrototypeOf(baseRepo, UserRepository.prototype);
+        return baseRepo;
+      },
+      inject: [DataSource],
+    },
+  ],
 })
 export class AuthModule {}
