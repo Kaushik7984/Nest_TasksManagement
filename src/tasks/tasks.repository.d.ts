aeb51f64e8bdbc6b7b15acd59e3f395bfc0f8f41
Kaushik Tapaniya <113747930+Kaushik7984@users.noreply.github.com>
2025-04-07 12:40:14 +0530

    Delete src/tasks directory

--- src/tasks/tasks.repository.d.ts
+++
@@ -1,8 +0,0 @@
-import { Repository } from 'typeorm';
-import { Task } from './dto/task.entity';
-import { CreateTaskDto } from './dto/create-task.dto';
-import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
-export declare class TasksRepository extends Repository<Task> {
-    getTask(filterDto: GetTasksFilterDto): Promise<Task[]>;
-    createTask(createTaskDto: CreateTaskDto): Promise<Task>;
-}
